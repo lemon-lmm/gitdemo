a2af2838d1b45e74cecf5429393c1f0098caa134
limengmeng <mistylim@163.com>
2019-11-21 20:13:33 +0800

    三次修改代码

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -4,7 +4,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("HelloWorld1");
-        System.out.println("韦观儿子");
-        System.out.println("为古纳儿子");
     }
 }
