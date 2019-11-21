3aa24b129450426ad2c1b81d2d97585ad08156f9
limengmeng <mistylim@163.com>
2019-11-21 20:11:07 +0800

    韦观儿子啊

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("HelloWorld1");
+        System.out.println("韦观儿子");
     }
 }
