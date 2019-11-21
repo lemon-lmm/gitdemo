686793e1eaecfe54a65606484937c4d57f0045f4
lemon-lmm <58014627+lemon-lmm@users.noreply.github.com>
2019-11-21 21:01:58 +0800

    Update HelloWorld.java

--- src/ main/ java/ com/HelloWorld.java
+++ src/ main/ java/ com/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("1111");
         System.out.println("HelloWorld1");
         System.out.println("dfsf");
-        System.out.println("1312423");
+        System.out.println("观儿子,爸爸在此,休得放肆!");
     }
 }
